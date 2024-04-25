5c97d013f33456b9d5fd9d2c2a5e48da50699ce9
Maddie Amelia Lewis <maddieamie@gmail.com>
2024-04-25 15:49:59 -0700

    game running as expected

--- ten_thousand/mock_roller.py
+++ ten_thousand/mock_roller.py
@@ -1,11 +1,17 @@
 from random import randint
 
 
+
 def default_roller():
     return randint(1, 6, ), randint(1, 6, )
 
 
-def play_dice(roller=default_roller):
+def mock_roller():
+    rollss = [(4, 4, 5, 2, 3, 1), (4, 2, 6, 4, 6, 5), (6, 4, 5, 2, 3, 1), (3, 2, 5, 4, 3, 3), (5, 2, 3, 2, 1, 4)]
+    return rollss.pop(0)
+
+
+def play_dice(roller=mock_roller):
 
     while True:
         print("Enter r to roll or q to quit")
@@ -23,7 +29,7 @@ def play_dice(roller=default_roller):
 
 
 if __name__ == "__main__":
-    rolls = [(1, 1), (3, 3), (2, 2)]
+    rolls = [(4, 4, 5, 2, 3, 1), (3, 3), (2, 2)]
 
     def mock_roller():
         return rolls.pop(0) if rolls else default_roller()
--- tests/version_2/test_sim_basic.py
+++ tests/version_2/test_sim_basic.py
@@ -1,33 +1,34 @@
 import pytest
-from flo import diff
-from ten_thousand.game import play
+from ten_thousand.flo import diff
+from ten_thousand.game import play, first_welcome
+from ten_thousand.game import GameLogic
 
 pytestmark = [pytest.mark.version_2]
 
 
 # @pytest.mark.skip("Pending")
 def test_quitter():
-    diffs = diff(play, path="tests/version_2/quitter.sim.txt")
+    diffs = diff(first_welcome, path="tests/version_2/quitter.sim.txt")
     assert not diffs, diffs
 
 
-@pytest.mark.skip("Pending")
+#@pytest.mark.skip("Pending")
 def test_one_and_done():
-    diffs = diff(play, path="tests/version_2/one_and_done.sim.txt")
+    diffs = diff(first_welcome, path="tests/version_2/one_and_done.sim.txt")
     assert not diffs, diffs
 
 
-@pytest.mark.skip("Pending")
+#@pytest.mark.skip("Pending")
 def test_single_bank():
     diffs = diff(
-        play, path="tests/version_2/bank_one_roll_then_quit.sim.txt"
+        first_welcome, path="tests/version_2/bank_one_roll_then_quit.sim.txt"
     )
     assert not diffs, diffs
 
 
-@pytest.mark.skip("Pending")
+#@pytest.mark.skip("Pending")
 def test_bank_first_for_two_rounds():
     diffs = diff(
-        play, path="tests/version_2/bank_first_for_two_rounds.sim.txt"
+        first_welcome, path="tests/version_2/bank_first_for_two_rounds.sim.txt"
     )
     assert not diffs, diffs
